433e0ff90b75795139fc8293b82686670ac71e12
Carwyn Ellis <carwynellis@gmail.com>
2019-08-03 17:59:00 +0100

    Added basic operators with tests
    * TODO - need to allow n * Vec3 or Vec3 * n - currently only the latter
    is supported

--- src/main/kotlin/uk/carwynellis/raytracing/Vec3.kt
+++ src/main/kotlin/uk/carwynellis/raytracing/Vec3.kt
@@ -1,10 +1,34 @@
 package uk.carwynellis.raytracing
 
+import kotlin.math.sqrt
+
 data class Vec3(val x: Double, val y: Double, val z: Double) {
     // Alias the x, y, z values since this class is also used to represent colour data.
     val r: Double = x
     val g: Double = y
     val b: Double = z
+
+    // Operator overloads for basic mathematical operators.
+    operator fun plus(that: Vec3): Vec3 =   Vec3(x + that.x, y + that.y, z + that.z)
+    operator fun minus(that: Vec3): Vec3 =  Vec3(x - that.x, y - that.y, z - that.z)
+    operator fun times(that: Vec3): Vec3 =  Vec3(x * that.x, y * that.y, z * that.z)
+    operator fun div(that: Vec3): Vec3 =    Vec3(x / that.x, y / that.y, z / that.z)
+    // Additional times and div operators that take a double.
+    // TODO - these operators allow for Vec3() * n but not for n * Vec3()...
+    operator fun times(n: Double) = Vec3(x * n, y * n, z * n)
+    operator fun div(n: Double) =   Vec3(x / n, y / n, z / n)
+
+    /**
+     * Computes the sum of each of the x, y, z components squared
+     */
+    fun squaredLength(): Double = (x * x) + (y * y) + (z * z)
+
+    /**
+     * Computes the vector length defined as the square root of x^2 + y^2 + z^2
+     */
+    fun length(): Double = sqrt(squaredLength())
+
+
 }
 
 // Unary operators
--- src/test/kotlin/uk/carwynellis/raytracing/Vec3Test.kt
+++ src/test/kotlin/uk/carwynellis/raytracing/Vec3Test.kt
@@ -2,7 +2,9 @@ package uk.carwynellis.raytracing
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import kotlin.math.sqrt
 
+// TODO - consider using matchers - something like hamkrest?
 class Vec3Test {
 
     @Test
@@ -13,6 +15,46 @@ class Vec3Test {
     @Test
     fun `unary plus should return Vec3 unchanged`() {
         assertEquals(Vec3(1.0, 2.0, 3.0), +Vec3(1.0, 2.0, 3.0))
+    }
+
+    @Test
+    fun `squaredLength should compute a valid result for a given Vec3`() {
+        assertEquals(14.0, Vec3(1.0, 2.0, 3.0).squaredLength())
+    }
+
+    @Test
+    fun `length should compute a valid result for a given Vec3`() {
+       assertEquals(sqrt(14.0), Vec3(1.0, 2.0, 3.0).length())
+    }
+
+    @Test
+    fun `plus operator should return a new Vec3 containing the sum of each component value`() {
+        assertEquals(Vec3(3.0, 3.0, 3.0), Vec3(1.0, 1.0, 1.0) + Vec3(2.0, 2.0, 2.0))
+    }
+
+    @Test
+    fun `minus operator should return a new Vec3 containing the difference between each component value`() {
+        assertEquals(Vec3(2.0, 2.0, 2.0), Vec3(3.0, 3.0, 3.0) - Vec3(1.0, 1.0, 1.0))
+    }
+
+    @Test
+    fun `times operator should return a new Vec3 containing the product of each component value`() {
+        assertEquals(Vec3(9.0, 9.0, 9.0), Vec3(3.0, 3.0, 3.0) * Vec3(3.0, 3.0, 3.0))
+    }
+
+    @Test
+    fun `div operator should return a new Vec3 containing the quotient of each component value`() {
+        assertEquals(Vec3(3.0, 3.0, 3.0), Vec3(9.0, 9.0, 9.0) / Vec3(3.0, 3.0, 3.0))
+    }
+
+    @Test
+    fun `times n operator should return a new Vec3 with each component multiplied by n`() {
+        assertEquals(Vec3(9.0, 9.0, 9.0), Vec3(3.0, 3.0, 3.0) * 3.0)
+    }
+
+    @Test
+    fun `div n operator should return a new Vec3 with each component divided by n`() {
+        assertEquals(Vec3(3.0, 3.0, 3.0), Vec3(9.0, 9.0, 9.0) / 3.0)
     }
 
 }
