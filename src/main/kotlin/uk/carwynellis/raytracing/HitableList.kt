55b9ff8ec56dddea3660eff08bd8d7f5414f875d
Carwyn Ellis <carwynellis@gmail.com>
2019-08-11 21:20:32 +0100

    Replace useage of drop(1) with sublist(1, list.size) in HitableList
    drop(1) is extremely slow since it appears to generate a new copy of the
    list. Replacing this with sublist is significantly faster since the
    result is backed by the original list.

--- src/main/kotlin/uk/carwynellis/raytracing/HitableList.kt
+++ src/main/kotlin/uk/carwynellis/raytracing/HitableList.kt
@@ -6,8 +6,8 @@ class HitableList(val hitables: List<Hitable>) : Hitable {
         tailrec fun loop(hs: List<Hitable>, closest: Double , hitAnything: Boolean, record: HitRecord?): HitRecord? {
             return if (hs.isNotEmpty()) {
                 when(val hitResult = hs.first().hit(r, tMin, closest)) {
-                    is HitRecord -> loop(hs.drop(1), hitResult.t, true, hitResult)
-                    else -> loop(hs.drop(1), closest, hitAnything, record)
+                    is HitRecord -> loop(hs.subList(1, hs.size), hitResult.t, true, hitResult)
+                    else -> loop(hs.subList(1, hs.size), closest, hitAnything, record)
                 }
             }
             else record
