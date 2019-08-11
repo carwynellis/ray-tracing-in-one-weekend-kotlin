7ca52e69a370ce608d80fb646b4516466a97486c
Carwyn Ellis <carwynellis@gmail.com>
2019-08-11 18:02:42 +0100

    Updated metals scene to demonstrate fuzziness on metals

--- src/main/kotlin/uk/carwynellis/raytracing/Scene.kt
+++ src/main/kotlin/uk/carwynellis/raytracing/Scene.kt
@@ -12,7 +12,7 @@ object Scene {
     val metalSpheres = HitableList(listOf(
         Sphere(Vec3(0.0, 0.0, -1.0), 0.5, Lambertian(Vec3(0.8, 0.3, 0.3))),
         Sphere(Vec3(0.0, -100.5, -1.0), 100.0, Lambertian(Vec3(0.8, 0.8, 0.0))),
-        Sphere(Vec3(1.0, 0.0, -1.0), 0.5, Metal(Vec3(0.8, 0.6, 0.2), 0.0)),
-        Sphere(Vec3(-1.0, 0.0, -1.0), 0.5, Metal(Vec3(0.8, 0.8, 0.8), 0.0))
+        Sphere(Vec3(1.0, 0.0, -1.0), 0.5, Metal(Vec3(0.8, 0.6, 0.2), 0.3)),
+        Sphere(Vec3(-1.0, 0.0, -1.0), 0.5, Metal(Vec3(0.8, 0.8, 0.8), 0.8))
     ))
 }
