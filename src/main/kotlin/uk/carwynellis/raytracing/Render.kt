42dee480ea27d8918ba1186f2fd558319c7f96c2
Carwyn Ellis <carwynellis@gmail.com>
2019-08-18 18:16:19 +0100

    Move colour function into Renderer and add parallelized render function
    * parallelizing using a parallelStream for now, which is a little lazy,
    but probably good enough here
    
    TODO - try parallelizing the render using co-routines for comparison

--- src/main/kotlin/uk/carwynellis/raytracing/Render.kt
+++ src/main/kotlin/uk/carwynellis/raytracing/Render.kt
@@ -8,7 +8,7 @@ import uk.carwynellis.raytracing.Vec3.Operators.times
 fun main() {
     val width = 1200
     val height = 800
-    val samples = 100
+    val samples = 10
 
     val scene = Scene.finalScene
 
@@ -31,39 +31,11 @@ fun main() {
 
     print("Rendering scene -   0% complete")
 
-    val imageData = renderer.renderScene()
+    val imageData = renderer.renderScenePar()
 
     println("Writing file: $filename...")
 
     ImageWriter(width, height, filename).writeImageData(imageData)
 
     println("Done.")
-}
-
-
-
-// When rendering some rays may may include a floating point error preventing them from being treated as 0. We increase
-// the minimum value we accept slightly which yields a smoother image without visible noise.
-const val IMAGE_SMOOTHING_LIMIT = 0.0001
-// Max number of times we recurse in the colour function.
-const val MAX_RECURSION_DEPTH = 50
-
-// TODO - can this be expressed as a tailrec function?
-fun colour(ray: Ray, world: Hitable, depth: Int): Vec3 {
-    fun backgroundColour(): Vec3 {
-        val unitDirection = ray.direction.unitVector()
-        val u = 0.5 * (unitDirection.y + 1)
-        return (1.0 - u) * Vec3(1.0, 1.0, 1.0) + u * Vec3(0.5, 0.7, 1.0)
-    }
-
-    val hitResult = world.hit(ray, IMAGE_SMOOTHING_LIMIT, Double.MAX_VALUE)
-
-    return hitResult?.let {
-        if (depth <  MAX_RECURSION_DEPTH) {
-            val scattered = hitResult.material.scatter(ray, hitResult)
-            hitResult.material.albedo * colour(scattered, world, depth + 1)
-        }
-        // Max recursion limit exceeded so we effectively return a no-op here.
-        else Vec3(0.0, 0.0, 0.0)
-    } ?: backgroundColour()
 }
--- src/main/kotlin/uk/carwynellis/raytracing/Renderer.kt
+++ src/main/kotlin/uk/carwynellis/raytracing/Renderer.kt
@@ -1,12 +1,40 @@
 package uk.carwynellis.raytracing
 
+import uk.carwynellis.raytracing.Vec3.Operators.times
 import kotlin.random.Random
+import kotlin.streams.toList
 
 class Renderer(private val camera: Camera,
                private val scene: Hitable,
                private val width: Int,
                private val height: Int,
                private val samples: Int) {
+
+    // When rendering some rays may may include a floating point error preventing them from being treated as 0. We
+    // increase the minimum value we accept slightly which yields a smoother image without visible noise.
+    private val imageSmoothingLimit = 0.0001
+    // Max number of times we recurse in the colour function.
+    private val maxRecursionDepth = 50
+
+    // Generates a gradated background colour where no object has been 'hit' by a ray.
+    private fun backgroundColour(ray: Ray): Vec3 {
+        val unitDirection = ray.direction.unitVector()
+        val u = 0.5 * (unitDirection.y + 1)
+        return (1.0 - u) * Vec3(1.0, 1.0, 1.0) + u * Vec3(0.5, 0.7, 1.0)
+    }
+
+    private fun colour(ray: Ray, world: Hitable, depth: Int): Vec3 {
+        val hitResult = world.hit(ray, imageSmoothingLimit, Double.MAX_VALUE)
+
+        return hitResult?.let {
+            if (depth <  maxRecursionDepth) {
+                val scattered = hitResult.material.scatter(ray, hitResult)
+                hitResult.material.albedo * colour(scattered, world, depth + 1)
+            }
+            // Max recursion limit exceeded so we effectively return a no-op here.
+            else Vec3(0.0, 0.0, 0.0)
+        } ?: backgroundColour(ray)
+    }
 
     private fun samplePixel(x: Double, y: Double): Vec3 {
         val u = (x + Random.nextDouble()) / width.toDouble()
@@ -22,12 +50,24 @@ class Renderer(private val camera: Camera,
         return sum / samples.toDouble()
     }
 
-    fun renderScene(): List<Vec3> {
-        return (height downTo 1).flatMap { j ->
-            val row = (1..width).map { i -> renderPixel(i, j) }
-            val percentComplete = ((height - j).toDouble() / (height-1).toDouble()) * 100
-            print("\rRendering scene - %3d%s complete".format(percentComplete.toInt(), "%"))
-            row
-        }
+    /**
+     * Single threaded scene renderer with progress indication.
+     */
+    fun renderScene(): List<Vec3> = (height downTo 1).flatMap { j ->
+        val row = (1..width).map { i -> renderPixel(i, j) }
+        val percentComplete = ((height - j).toDouble() / (height-1).toDouble()) * 100
+        print("\rRendering scene - %3d%s complete".format(percentComplete.toInt(), "%"))
+        row
     }
+
+    /**
+     * Parallelized renderer using a parallelStream.
+     *
+     * This is quite lazy but probably good enough here.
+     *
+     * TODO - try this using co-routines for comparison.
+     */
+    fun renderScenePar(): List<Vec3> = (height downTo 1).toList().parallelStream().map { j ->
+        (1..width).map { i -> renderPixel(i, j) }
+    }.toList().flatten()
 }
